c9ed64c25e91952773148c9e5d9cef0baa38a26f
Pedro Schmitt <pedromartins.bsb@gmail.com>
2022-01-06 23:24:55 -0300

    Set better names

--- app/controllers/binance_chart_controller.rb
+++ app/controllers/binance_chart_controller.rb
@@ -2,14 +2,14 @@ class BinanceChartController < ApplicationController
   def index
     @symbol = 'ADABNB'
     @data = BinanceService::Klines.new(symbol: @symbol, interval: '1h').call
-    @data_last_50 = @data.take(50)
-    @opens = @data_last_50.collect(&:second).map(&:to_f)
-    @last_5_days_moving_average = moving_average(@opens, 5, 8)
+    @data_last50 = @data.take(50)
+    @opens_values = @data_last_50.collect(&:second).map(&:to_f)
+    @last_5_days_moving_average = moving_average(@opens_values, 5, 8)
   end
 
   private
 
-  def moving_average(a, ndays, precision)
-    a.each_cons(ndays).map { |e| e.reduce(&:+).fdiv(ndays).round(precision) }
+  def moving_average(array, ndays, precision)
+    array.each_cons(ndays).map { |e| e.reduce(&:+).fdiv(ndays).round(precision) }
   end
 end
